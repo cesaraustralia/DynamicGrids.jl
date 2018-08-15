ed5bbce3893ecafc8884f957f2b3ec8064afe41d
Rafael Schouten <rafaelschouten@gmail.com>
2018-08-16 00:05:49 +1000

    fix repl

--- src/outputs/repl.jl
+++ src/outputs/repl.jl
@@ -9,38 +9,9 @@ REPLOutput{X}(frames::AbstractVector; fps=25, color=:white) where X = begin
     REPLOutput{X,typeof.((frames, fps, t, color))...}(frames, fps, t, [true], [false], [1,1], color)
 end
 
-
 initialize(output::REPLOutput, args...) = begin
     output.displayoffset .= (1, 1)
-    # Terminal.enableRawMode()
     @async movedisplay(output)
-end
-
-# finalize(output::REPLOutput, args...) = Terminal.disableRawMode()
-
-movedisplay(output) = begin
-    while is_ok(output)
-        c = Terminal.readKey()
-        c == "Up"      && move_y!(output, -1)
-        c == "Down"    && move_y!(output, 1)
-        c == "Left"    && move_x!(output, -1)
-        c == "Right"   && move_x!(output, 1)
-        c == "PgUp"    && move_y!(output, -10)
-        c == "PgDown"  && move_y!(output, 10)
-        c == '\x1b'    && set_ok(output, false)
-        c in ["Ctrl-C"]  && set_ok(output, false)
-    end
-end
-
-move_y!(output, n) = begin
-    dispy, dispx = displaysize(Base.STDOUT)
-    y, x = size(output[1])
-    output.displayoffset[1] = max(0, min(y-dispy, output.displayoffset[1] + 2n))
-end
-move_x!(output, n) = begin
-    dispy, dispx = displaysize(Base.STDOUT)
-    y, x = size(output[1])
-    output.displayoffset[2] = max(0, min(x-dispx, output.displayoffset[2] + 2n))
 end
 
 """
@@ -57,6 +28,8 @@ show_frame(output::REPLOutput, t) = begin
     delay(output)
     is_ok(output)
 end
+
+is_async(o::REPLOutput) = true
 
 """
     Base.show(io::IO, output::REPLOutput)
@@ -77,9 +50,47 @@ function replframe(output, t, ystep, xstep, f)
     youtput, xoutput = outputsize = size(frame)
     yoffset, xoffset = output.displayoffset
 
-    yrange = max(1, yoffset):min(youtput-1, ystep * (dispy + yoffset - 1))
-    xrange = max(1, xoffset):min(xoutput, xstep * (dispx + xoffset - 1))
+    yrange = max(1, ystep * yoffset):min(youtput, ystep * (dispy + yoffset))
+    xrange = max(1, xstep * xoffset):min(xoutput, xstep * (dispx + xoffset))
     let frame=frame, yrange=yrange, xrange=xrange
         f(frame, 0.5, yrange, xrange)
     end
 end
+
+
+movedisplay(output) = begin
+    while is_ok(output)
+        c = Terminal.readKey()
+        c == "Up"      && move_y!(output, -1)
+        c == "Down"    && move_y!(output, 1)
+        c == "Left"    && move_x!(output, -1)
+        c == "Right"   && move_x!(output, 1)
+        c == "PgUp"    && move_y!(output, -10)
+        c == "PgDown"  && move_y!(output, 10)
+        c == '\x1b'    && set_ok(output, false)
+        c in ["Ctrl-C"]  && set_ok(output, false)
+    end
+end
+
+move_y!(output::REPLOutput{:block}, n) = begin
+    dispy, dispx = displaysize(Base.STDOUT)
+    y, x = size(output[1])
+    output.displayoffset[1] = max(0, min(y ÷ 2 - dispy, output.displayoffset[1] + 4n))
+end
+move_x!(output::REPLOutput{:block}, n) = begin
+    dispy, dispx = displaysize(Base.STDOUT)
+    y, x = size(output[1])
+    output.displayoffset[2] = max(0, min(x - dispx, output.displayoffset[2] + 8n))
+end
+move_y!(output::REPLOutput{:braile}, n) = begin
+    dispy, dispx = displaysize(Base.STDOUT)
+    y, x = size(output[1])
+    output.displayoffset[1] = max(0, min(y ÷ 4 - dispy, output.displayoffset[1] + 4n))
+end
+move_x!(output::REPLOutput{:braile}, n) = begin
+    dispy, dispx = displaysize(Base.STDOUT)
+    y, x = size(output[1])
+    output.displayoffset[2] = max(0, min(x ÷ 2 - dispx, output.displayoffset[2] + 8n))
+end
+
+
