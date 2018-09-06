de0f96f1df7154a2ab199d437095dd88ce192f04
Rafael Schouten <rafaelschouten@gmail.com>
2018-09-06 21:56:16 +1000

    o -> ouput

--- src/outputs/repl.jl
+++ src/outputs/repl.jl
@@ -1,51 +1,60 @@
-"A wrapper for [`ArrayOutput`](@ref) that is displayed as asccii blocks in the REPL."
+"""
+A simple output that is displayed directly in the REPL.
+
+### Arguments:
+- `frames::AbstractVector`: Vector of frames
+
+### Keyword Arguments:
+- `fps`: frames per second
+- `showmax_fps`: maximum displayed frames per second
+- `store::Bool`: save frames or not
+- `color`: a color name symbol from Crayons.jl
+
+Pass `:braile` or `:block` to the constructor:
+```julia
+REPLOutput{:block}(init)
+```
+"""
 @premix struct SubType{X} end
-@Ok @FPS @Frames @SubType mutable struct REPLOutput{C} <: AbstractOutput{T} 
+@Ok @FPS @Frames @SubType mutable struct REPLOutput{C} <: AbstractOutput{T}
     displayoffset::Array{Int}
     color::C
 end
-REPLOutput{X}(frames::AbstractVector; fps=25, color=:white) where X = 
-    REPLOutput{X,typeof.((frames, fps, 0.0, color))...}(frames, fps, 0.0, [false], [1,1], color)
+REPLOutput{X}(frames::AbstractVector; fps=25, showmax_fps=100, store=false, color=:white) where X =
+    REPLOutput{X,typeof.((frames, fps, 0.0, 0, color))...}(
+               frames, fps, showmax_fps, 0.0, 0, store, [false], [1,1], color)
 
-initialize(output::REPLOutput, args...) = begin
-    output.displayoffset .= (1, 1)
-    @async movedisplay(output)
-    output.timestamp = time()
+initialize(o::REPLOutput, args...) = begin
+    o.displayoffset .= (1, 1)
+    @async movedisplay(o)
+    o.timestamp = time()
 end
 
 """
-    show_frame(output::REPLOutput, t)
+    show_frame(o::REPLOutput, t)
 Extends show_frame from [`ArrayOuput`](@ref) by also printing to the REPL.
 """
-show_frame(output::REPLOutput, t) = 
-    try
-        out = replshow(output, t)
-        REPLGamesBase.put([0,0], output.color, out)
+show_frame(o::REPLOutput, t) = 
+    try 
+        out = replshow(o, curframe(o, t)) 
+        REPLGamesBase.put([0,0], o.color, out) 
+        REPLGamesBase.put([0,0], o.color, string(t)) 
     catch err
-        set_running(output, false)
+        set_running(o, false)
         throw(err)
     end
 
 is_async(o::REPLOutput) = true
 
-"""
-    Base.show(io::IO, output::REPLOutput)
-Print the last frame of a simulation in the REPL.
-"""
-Base.show(io::IO, output::REPLOutput) = begin
-    println(io, typeof(output))
-    length(output) == 0 || print(repl_frame(output[end]))
-end
+replshow(o::REPLOutput{:braile}, t) = replframe(o, t, 4, 2, brailize)
+replshow(o::REPLOutput{:block}, t) = replframe(o, t, 2, 1, blockize)
 
-replshow(output::REPLOutput{:braile}, t) = replframe(output, t, 4, 2, brailize) 
-replshow(output::REPLOutput{:block}, t) = replframe(output, t, 2, 1, blockize) 
-
-function replframe(output, t, ystep, xstep, f)
-    frame = output[t]
+function replframe(o, i, ystep, xstep, f)
+    frame = o[i]
     # Limit output area to available terminal size.
     dispy, dispx = dispsize = displaysize(stdout)
     youtput, xoutput = outputsize = size(frame)
-    yoffset, xoffset = output.displayoffset
+    yoffset, xoffset = o.displayoffset
 
     yrange = max(1, ystep * yoffset):min(youtput, ystep * (dispy + yoffset - 1))
     xrange = max(1, xstep * xoffset):min(xoutput, xstep * (dispx + xoffset - 1))
@@ -55,37 +64,37 @@ function replframe(output, t, ystep, xstep, f)
 end
 
 
-movedisplay(output) = 
-    while is_running(output)
+movedisplay(o) =
+    while is_running(o)
         c = REPLGamesBase.readKey()
-        c == "Up"      && move_y!(output, -1)
-        c == "Down"    && move_y!(output, 1)
-        c == "Left"    && move_x!(output, -1)
-        c == "Right"   && move_x!(output, 1)
-        c == "PgUp"    && move_y!(output, -10)
-        c == "PgDown"  && move_y!(output, 10)
-        c in ["Ctrl-C"]  && set_running(output, false)
+        c == "Up"      && move_y!(o, -1)
+        c == "Down"    && move_y!(o, 1)
+        c == "Left"    && move_x!(o, -1)
+        c == "Right"   && move_x!(o, 1)
+        c == "PgUp"    && move_y!(o, -10)
+        c == "PgDown"  && move_y!(o, 10)
+        c in ["Ctrl-C"]  && set_running(o, false)
     end
 
-move_y!(output::REPLOutput{:block}, n) = begin
+move_y!(o::REPLOutput{:block}, n) = begin
     dispy, dispx = displaysize(stdout)
-    y, x = size(output[1])
-    output.displayoffset[1] = max(0, min(y ÷ 2 - dispy, output.displayoffset[1] + 4n))
+    y, x = size(o[1])
+    o.displayoffset[1] = max(0, min(y ÷ 2 - dispy, o.displayoffset[1] + 4n))
 end
-move_x!(output::REPLOutput{:block}, n) = begin
+move_x!(o::REPLOutput{:block}, n) = begin
     dispy, dispx = displaysize(stdout)
-    y, x = size(output[1])
-    output.displayoffset[2] = max(0, min(x - dispx, output.displayoffset[2] + 8n))
+    y, x = size(o[1])
+    o.displayoffset[2] = max(0, min(x - dispx, o.displayoffset[2] + 8n))
 end
-move_y!(output::REPLOutput{:braile}, n) = begin
+move_y!(o::REPLOutput{:braile}, n) = begin
     dispy, dispx = displaysize(stdout)
-    y, x = size(output[1])
-    output.displayoffset[1] = max(0, min(y ÷ 4 - dispy, output.displayoffset[1] + 4n))
+    y, x = size(o[1])
+    o.displayoffset[1] = max(0, min(y ÷ 4 - dispy, o.displayoffset[1] + 4n))
 end
-move_x!(output::REPLOutput{:braile}, n) = begin
+move_x!(o::REPLOutput{:braile}, n) = begin
     dispy, dispx = displaysize(stdout)
-    y, x = size(output[1])
-    output.displayoffset[2] = max(0, min(x ÷ 2 - dispx, output.displayoffset[2] + 8n))
+    y, x = size(o[1])
+    o.displayoffset[2] = max(0, min(x ÷ 2 - dispx, o.displayoffset[2] + 8n))
 end
 
 
