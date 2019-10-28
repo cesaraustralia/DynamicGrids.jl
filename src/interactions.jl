c4e74225f4b66bfa3b277a2539f8df64d4582d30
Rafael Schouten <rafaelschouten@gmail.com>
2019-10-28 15:29:55 +1100

    clean up abstract types

--- src/interactions.jl
+++ src/interactions.jl
@@ -1,23 +1,7 @@
-abstract type AbstractInteraction{Keys} end
+abstract type Interaction{Keys} <: Rule end
 
-keys(::AbstractInteraction{Keys}) where Keys = Keys
+keys(::Interaction{Keys}) where Keys = Keys
 
-# (::Type{T(args...) where T<:AbstactInteraction, Keys} = T{Keys,typeof.(args)...}(args...)
-
-# Provide a constructor for generic rule reconstruction
-# ConstructionBase.constructorof(::Type{T}) where T<:AbstractInteracton{Keys} where Keys = Interactive{Keys} 
-
-# val(rm::abstraInteraction) = rm.val
-
-
-show(io::IO, interaction::AbstractInteraction{Keys}) where Keys = begin
-    indent = get(io, :indent, "")
-    printstyled(io, indent, Base.nameof(typeof(interaction)); color=:red)
-    printstyled(io, indent, Keys; color=:yellow)
-    if nfields(interaction) > 0
-        printstyled(io, " :\n"; color=:red)
-        for fn in fieldnames(typeof(interaction))
-            println(io, indent, "    ", fn, " = ", repr(getfield(interaction, fn)))
-        end
-    end
-end
+# Provide a constructor for generic rule reconstruction in Flatten.jl and Setfield.jl
+ConstructionBase.constructorof(::Type{T}) where T<:AbstractInteracton{Keys} where Keys = 
+    T{Keys} 
