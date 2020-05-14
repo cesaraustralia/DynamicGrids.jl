2410f5ba412129b712e7de897cef1a8c764caae4
Rafael Schouten <rafaelschouten@gmail.com>
2020-05-14 12:28:04 +1000

    finsh mask tests

--- test/mask.jl
+++ test/mask.jl
@@ -1,28 +1,27 @@
 using DynamicGrids, Test
 
-init = [1.0 4.0 7.0;
-        2.0 5.0 8.0;
-        3.0 6.0 9.0]
+init = [0.0 4.0 0.0
+        0.0 5.0 8.0
+        3.0 6.0 0.0]
 
-mask = Bool[0 1 0;
-            0 1 1;
+mask = Bool[0 1 0
+            0 1 1
             1 1 0]
 
-struct DoNothingRule{R,W} <: Rule{R,W} end
-DynamicGrids.applyrule(::DoNothingRule, data, state, args...) = state
+struct AddOneRule{R,W} <: Rule{R,W} end
+DynamicGrids.applyrule(::AddOneRule, data, state, args...) = state + 1
 
-rules = Ruleset(DoNothingRule{:_default_,:_default_}(); init=init, mask=mask)
+rules = Ruleset(AddOneRule{:_default_,:_default_}(); init=init, mask=mask)
 
-output = ArrayOutput(init, 8)
-sim!(output, rules; tspan=(1, 8))
+output = ArrayOutput(init, 3)
+sim!(output, rules; tspan=(1, 3))
 
-@test output[1] == [1.0 4.0 7.0;
-                    2.0 5.0 8.0;
-                    3.0 6.0 9.0]
-
-# TODO: is this how mask should really work?
-# Should it just assume the init is already masked?
-# Otherwise we have to do extra writes every frame.
-@test_broken output[2] == [0.0 4.0 0.0;
-                           0.0 5.0 8.0;
-                           3.0 6.0 0.0]
+@test output[1] == [0.0 4.0 0.0
+                    0.0 5.0 8.0
+                    3.0 6.0 0.0]
+@test output[2] == [0.0 5.0 0.0
+                    0.0 6.0 9.0
+                    4.0 7.0 0.0]
+@test output[3] == [0.0 6.0 0.0
+                    0.0 7.0 10.0
+                    5.0 8.0 0.0]
