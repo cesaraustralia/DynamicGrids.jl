f2537fd062af7689b923ea494e560a3e76519b77
byronbest <43890914+byronbest@users.noreply.github.com>
2020-09-07 17:43:38 -0700

    sp otherise

--- src/outputs/output.jl
+++ src/outputs/output.jl
@@ -2,7 +2,7 @@
 """
 Outputs are store or display simulation results, usually
 as a vector of grids, one for each timestep - but they may also
-sum, combine or otherise manipulate the simulation grids to improve
+sum, combine or otherwise manipulate the simulation grids to improve
 performance, reduce memory overheads or similar.
 
 Simulation outputs are decoupled from simulation behaviour,
