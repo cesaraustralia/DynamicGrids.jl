4b487de5f3bf1d7e67ce19bfb9bee1dcc6f2a8ee
Rafael Schouten <rafaelschouten@gmail.com>
2021-06-16 18:45:18 +1000

    more tests of TransformedGrid

--- test/transformed.jl
+++ test/transformed.jl
@@ -52,3 +52,40 @@ end
     @test transformed_output[2] == 24
     @test transformed_output[3] == 44
 end
+
+@testset "Padded grids" begin
+    init =  Bool[
+             0 0 0 0 0 0 0
+             0 0 0 0 1 1 1
+             0 0 0 0 0 0 1
+             0 0 0 0 0 1 0
+             0 0 0 0 0 0 0
+             0 0 0 0 0 0 0
+            ]
+    test2 = Bool[
+             0 0 0 0 0 1 0
+             0 0 0 0 0 1 1
+             0 0 0 0 1 0 1
+             0 0 0 0 0 0 0
+             0 0 0 0 0 0 0
+             0 0 0 0 0 0 0
+            ]
+    test3 = Bool[
+             0 0 0 0 0 1 1
+             0 0 0 0 1 0 1
+             0 0 0 0 0 0 1
+             0 0 0 0 0 0 0
+             0 0 0 0 0 0 0
+             0 0 0 0 0 0 0
+            ]
+
+    # Sum the last column in the grid
+    output = TransformedOutput(A -> sum(view(A, :, 7)), init; tspan=1:3)
+    sim!(output, Life())
+    output == [2, 2, 3]
+
+    # Sum the first row in the :a grid of the NamedTuple 
+    output = TransformedOutput(gs -> sum(view(gs[:a], 1, :)), (a=init,); tspan=1:3)
+    sim!(output, Life{:a}())
+    output == [0, 1, 2]
+end
