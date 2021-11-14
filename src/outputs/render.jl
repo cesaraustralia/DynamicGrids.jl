b76f28f6ad998011161d76b4ffbc40825fb9bea0
rafaqz <rafaelschouten@gmail.com>
2021-11-14 15:40:36 +0100

    start 1d renderer

--- src/outputs/render.jl
+++ src/outputs/render.jl
@@ -55,18 +55,31 @@ function render!(
     data::AbstractSimData{S}, grid::AbstractArray;
     name=nothing, time=currenttime(data), accessor=nothing,
     minval=minval(o), maxval=maxval(o),
-) where S<:Tuple
-    for I in CartesianIndices(map(Base.OneTo, S))
-        # If there is a specific accessor function, use it. This is used in 
-        # visualisations where multiple images come form a single grid holding
-        # multi-field objects
+) where S<:Tuple{Y,X} where {Y,X}
+    for j in 1:X, i in 1:Y
         accessor = accessor isa Nothing ? DynamicGrids.accessor(ig) : accessor
-        # Get a value from the grid object, maybe with an accessor function
-        @inbounds grid_obj = grid[I]
+        @inbounds grid_obj = grid[i, j]
         val = _access_grid_object(accessor, grid_obj)
-        # Creta and rgb pixel for the cell.
-        pixel = to_rgb(cell_to_pixel(ig, mask(o), minval, maxval, data, val, Tuple(I)))
-        @inbounds imagebuffer[I] = pixel
+        pixel = to_rgb(cell_to_pixel(ig, mask(o), minval, maxval, data, val, (i, j)))
+        @inbounds imagebuffer[i, j] = pixel
+    end
+    _rendertext!(imagebuffer, textconfig(o), name, time)
+    return imagebuffer
+end
+function render!(
+    imagebuffer, ig::SingleGridRenderer, o::ImageOutput, 
+    data::AbstractSimData{S}, grid::AbstractArray;
+    name=nothing, time=currenttime(data), accessor=nothing,
+    minval=minval(o), maxval=maxval(o),
+) where S<:Tuple{L} where {L}
+    f = currentframe(data)
+    for i in 1:L
+        accessor = accessor isa Nothing ? DynamicGrids.accessor(ig) : accessor
+        @inbounds grid_obj = grid[i]
+        val = _access_grid_object(accessor, grid_obj)
+        pixel = to_rgb(cell_to_pixel(ig, mask(o), minval, maxval, data, val, (i,)))
+        # Image rows are simulation frames
+        @inbounds imagebuffer[f, i] = pixel
     end
     _rendertext!(imagebuffer, textconfig(o), name, time)
     return imagebuffer
@@ -77,7 +90,7 @@ end
 
     Image(f=identity; scheme=ObjectScheme(), zerocolor=nothing, maskcolor=nothing)
 
-Converts output grids to an image with a colorsheme.
+Converts output grids to a colorsheme.
 
 # Arguments
 
