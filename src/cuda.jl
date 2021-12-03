2268daa0e57e5ecbb69b72b31cf2354b354ddbf8
Rafael Schouten <rafaelschouten@gmail.com>
2021-12-04 03:03:42 +1100

    fix performance of CuGPU adapt

--- src/cuda.jl
+++ src/cuda.jl
@@ -23,9 +23,32 @@ CuGPU() = CuGPU{32}()
 kernel_setup(::CuGPU{N}) where N = CUDAKernels.CUDADevice(), (N, N)
 
 # Adapt method for DynamicGrids objects
-function Adapt.adapt_structure(to, x::Union{AbstractSimData,GridData,Rule})
-    Flatten.modify(A -> adapt(to, A), x, Union{CuArray,Array,AbstractDimArray}, Union{SArray,Function})
+function Adapt.adapt_structure(to, x::AbstractSimData)
+    @set! x.grids = map(g -> Adapt.adapt(to, g), x.grids)
+    @set! x.extent = Adapt.adapt(to, x.extent)
+    return x
 end
+
+function Adapt.adapt_structure(to, x::GridData)
+    @set! x.source = Adapt.adapt(to, x.source)
+    @set! x.source = Adapt.adapt(to, x.source)
+    @set! x.mask = Adapt.adapt(to, x.mask)
+    @set! x.dest = Adapt.adapt(to, x.dest)
+    @set! x.sourcestatus = Adapt.adapt(to, x.sourcestatus)
+    @set! x.deststatus = Adapt.adapt(to, x.deststatus)
+    return x
+end
+
+function Adapt.adapt_structure(to, x::AbstractExtent)
+    @set! x.init = _adapt_x(to, init(x))
+    @set! x.mask = _adapt_x(to, mask(x))
+    @set! x.aux = _adapt_x(to, aux(x))
+    return x
+end
+
+_adapt_x(to, A::AbstractArray) = Adapt.adapt(to, A)
+_adapt_x(to, nt::NamedTuple) = map(A -> Adapt.adapt(to, A), nt)
+_adapt_x(to, nt::Nothing) = nothing
 
 # Adapt output frames to GPU
 # TODO: this may be incorrect use of Adapt.jl, as the Output
@@ -33,20 +56,17 @@ end
 # frames are still held in a regular Array.
 function Adapt.adapt_structure(to, o::Output)
     frames = map(o.frames) do f
-        if f isa NamedTuple
-            a = map(g -> adapt(to, g), f)
-        else
-            adapt(to, f)
-        end
+        _adapt_x(to, f)
     end
-    @set o.extent = adapt(to, o.extent)
-    @set o.frames = frames
+    @set! o.extent = adapt(to, o.extent)
+    @set! o.frames = frames
+    return o
 end
 
 # _proc_setup
 # Convert all arrays in SimData to CuArrays
 @noinline function _proc_setup(::CuGPU, simdata::AbstractSimData) 
-    Flatten.modify(CuArray, simdata, Union{Array,BitArray}, Union{CuArray,SArray,Dict,Function})
+    Adapt.adapt(CuArray, simdata)
 end
 
 _copyto_output!(outgrid, grid::GridData, proc::GPU) = copyto!(outgrid, gridview(grid))
