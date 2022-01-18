ec8830f5e3de8040be0f8c6706ad8fc190661ea5
ederag <edera@gmx.fr>
2022-01-18 16:50:58 +0100

    Update src/outputs/textconfig.jl
    Co-authored-by: Rafael Schouten <rafaelschouten@gmail.com>

--- src/outputs/textconfig.jl
+++ src/outputs/textconfig.jl
@@ -8,7 +8,7 @@ Text configuration for printing timestep and grid name on the image.
 
 # Arguments / Keywords
 
-- `font`: `FreeTypeAbstraction.FTFont` (direct), or `String` (font name to look for).
+- `font`: A `FreeTypeAbstraction.FTFont`, or a `String` with the font name to look for. The `FTFont` may load more quickly.
 - `namepixels` and `timepixels`: the pixel size of the font.
 - `timepos` and `namepos`: tuples that set the label positions, in `Int` pixels.
 - `fcolor` and `bcolor`: the foreground and background colors, as `ARGB32`.
