7ca66bc1ca005dd6d235e68007d8e48a34bab8df
ederag <edera@gmx.fr>
2022-01-27 14:07:10 +0100

    test passing String or FTFont to TextConfig

--- test/textconfig.jl
+++ test/textconfig.jl
@@ -1,6 +1,19 @@
-using DynamicGrids, Test
+using DynamicGrids, FreeTypeAbstraction, Test
 
 @testset "Fonts" begin
+	@test DynamicGrids.autofont() isa String
+	name = DynamicGrids.autofont()
+	face = FreeTypeAbstraction.findfont(name)
+ 	@testset "TextConfig accepts font as String" begin
+		@test name isa String
+		textconfig = TextConfig(; font=name)
+		@test textconfig.face isa FreeTypeAbstraction.FTFont
+	end
+ 	@testset "TextConfig accepts font as FTFont" begin
+		@test face isa FreeTypeAbstraction.FTFont
+		textconfig = TextConfig(; font=face)
+		@test textconfig.face === face
+	end
 	@test_throws ArgumentError TextConfig(; font="not_a_font")
 	@test_throws ArgumentError TextConfig(; font=:not_a_string)
 end
