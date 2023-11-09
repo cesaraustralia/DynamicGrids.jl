955fc8a00823b261b289cf9c6f60e07914a83c85
rafaqz <rafaelschouten@gmail.com>
2023-11-09 22:44:41 +0100

    more cuda

--- ext/DynamicGridsCUDAExt.jl
+++ ext/DynamicGridsCUDAExt.jl
@@ -8,7 +8,7 @@ DynamicGrids.kernel_setup(::CuGPU{N}) where N = CUDA.CUDAKernels.CUDABackend(), 
 
 # _proc_setup
 # Convert all arrays in SimData to CuArrays
-@noinline function DynamicGrids._proc_setup(::CuGPU, simdata::AbstractSimData) 
+@noinline function DynamicGrids._proc_setup(::CuGPU, simdata::DynamicGrids.AbstractSimData) 
     Adapt.adapt(CuArray, simdata)
 end
 
@@ -16,7 +16,7 @@ end
 for (f, op) in DynamicGrids.ATOMIC_OPS
     atomic_f = Symbol(:atomic_, f)
     @eval begin
-        function ($f)(d::GridData{<:WriteMode,<:Any,R}, ::CuGPU, x, I...) where R
+        function ($f)(d::DynamicGrids.GridData{<:DynamicGrids.WriteMode,<:Any,R}, ::CuGPU, x, I...) where R
             A = parent(dest(d))
             i = Base._to_linear_index(A, (I .+ R)...)
             (CUDA.$atomic_f)(pointer(A, i), x)
